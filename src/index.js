10d31df0c0f8667bf8d44108ceaea1adf60f9b12
Andrey Popp <8mayday@gmail.com>
2015-04-12 21:29:10 +0300

    Gardening

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ export default function autobind(...args) {
 }
 
 /**
-* Use boundMethod to bind all methods on the target.prototype
-*/
+ * Use boundMethod to bind all methods on the target.prototype
+ */
 function boundClass(target) {
   // (Using reflect to get all keys including symbols)
   Reflect.ownKeys(target.prototype).forEach(key => {
@@ -40,32 +40,32 @@ function boundClass(target) {
 }
 
 /**
-* Return a descriptor removing the value and returning a getter
-* The getter will return a .bind version of the function
-* and memoize the result against a symbol on the instance
-*/
+ * Return a descriptor removing the value and returning a getter
+ * The getter will return a .bind version of the function
+ * and memoize the result against a symbol on the instance
+ */
 function boundMethod(target, key, descriptor) {
   let _key;
   let fn = descriptor.value;
 
   if (typeof fn !== 'function') {
-    throw new Error('@autobind decorator can only be applied to methods not: ' + typeof fn);
+    throw new Error(`@autobind decorator can only be applied to methods not: ${typeof fn}`);
   }
 
   if (typeof key === 'string') {
     // Add the key to the symbol name for easier debugging
-    _key = Symbol('@autobind method: ' + key);
+    _key = Symbol(`@autobind method: ${key}`);
   } else if (typeof key === 'symbol') {
     // A symbol cannot be coerced to a string
     _key = Symbol('@autobind method: (symbol)');
   } else {
-    throw new Error('Unexpected key type: ' + typeof key);
+    throw new Error(`Unexpected key type: ${typeof key}`);
   }
 
   return {
     configurable: true, // must be true or we could not be changing it
-    get () {
-      if (! this.hasOwnProperty(_key)) {
+    get() {
+      if (!this.hasOwnProperty(_key)) {
         this[_key] = fn.bind(this);
       }
       return this[_key];
