4f7db173e427e21753c90cb07fb84daf908b2f70
Steve Mao <maochenyan@gmail.com>
2016-09-20 17:43:14 +1000

    fix "Out of stack space" on ie11
    fix #23

--- src/index.js
+++ src/index.js
@@ -65,21 +65,27 @@ function boundMethod(target, key, descriptor) {
     throw new Error(`@autobind decorator can only be applied to methods not: ${typeof fn}`);
   }
 
+  // In IE11 calling Object.defineProperty has a side-effect of evaluating the
+  // getter for the property which is being replaced. This causes infinite
+  // recursion and an "Out of stack space" error.
+  let definingProperty = false;
+
   return {
     configurable: true,
     get() {
-      if (this === target.prototype || this.hasOwnProperty(key)) {
+      if (definingProperty || this === target.prototype || this.hasOwnProperty(key)) {
         return fn;
       }
 
       let boundFn = fn.bind(this);
+      definingProperty = true;
       Object.defineProperty(this, key, {
         value: boundFn,
         configurable: true,
         writable: true
       });
+      definingProperty = false;
       return boundFn;
     }
   };
 }
-
