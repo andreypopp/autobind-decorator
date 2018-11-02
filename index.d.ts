76b9c8bfc7572a435c5375cce1dcd70e0bb7f090
Vladimir Kravchenko <Vladimir.Kravchenko@playtech.com>
2018-11-02 18:37:57 +0200

    Fixed typescript defenition for a new methods

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,6 @@
 declare const autobind: ClassDecorator & MethodDecorator;
+
+export declare const boundMethod: MethodDecorator;
+export declare const boundClass: ClassDecorator;
+
 export default autobind;
